b23cfd9c037dccb5b043254441263ec89cf42b29
Billy Stevens <contact@wasv.me>
2021-05-30 18:39:45 -0400

    Adds animated sprite.

--- src/main.c
+++ src/main.c
@@ -3,18 +3,21 @@
 
 #include <string.h>
 
-const uint32_t sprite_x = 104;
-const uint32_t sprite_y = 32;
-const uint32_t sprite_pallete = 0;
-const uint32_t sprite_id = 512;
+const uint16_t sprite_pallete = 0;
+const uint16_t sprite_id = 512;
 
 int main(void) {
+    int16_t sprite_x = 104;
+    int16_t sprite_y = 45;
+
+    // Initialize tile and pallete data.
     memcpy(&tile4_obj[512], spriteTiles, spriteTilesLen);
     memcpy(&pal_obj_bank[0], spritePal, spritePalLen);
 
     // Initialize all sprites
     oam_init(128);
 
+    // Set sprite attributes.
     obj_attr_t *sprite = &obj_attr[0];
     BFN_SET2(sprite->attr0, ATTR0_SQUARE, ATTR0_SHAPE);
     BFN_SET2(sprite->attr1, ATTR1_SIZE_32, ATTR1_SIZE);
@@ -25,27 +28,34 @@ int main(void) {
     BFN_SET(sprite->attr0, sprite_y, ATTR0_Y);
     BFN_SET(sprite->attr1, sprite_x, ATTR1_X);
 
+    // Set sprite as visible.
     BFN_SET2(sprite->attr0, ATTR0_REG, ATTR0_MODE);
 
     // Write into the I/O registers, setting video display parameters.
     SetMode(BG2_ENABLE | MODE_3 | OBJ_ENABLE | OBJ_MAP_1D);
 
-    uint8_t blink = 0;
+    int8_t sprite_dy = -1;
+    uint32_t frame = 0;
+
+    // Write pixel colours into VRAM
+    for (int i = 0; i < 80; i++) {
+        screen->mode3[i][115] = RGB15(0, 0, 31);
+        screen->mode3[i][120] = RGB15(0, 31, 0);
+        screen->mode3[i][125] = RGB15(31, 0, 0);
+    }
     // Repeat forever
     while (1) {
-        // Write pixel colours into VRAM
-        for (int i = 0; i < 80; i++) {
-            screen->mode3[i][115] = RGB15(0, 0, 31);
-            screen->mode3[i][120] = RGB15(0, 31, 0);
-            screen->mode3[i][121] = blink * RGB15(31, 31, 31);
-            screen->mode3[i][125] = RGB15(31, 0, 0);
-        }
-        if (KeyPressed(ANY)) {
-            blink = 1;
-        } else {
-            blink = 0;
+        if (frame % 120 == 0) {
+            if (sprite_y > 48) {
+                sprite_dy = -1;
+            } else if (sprite_y < 0) {
+                sprite_dy = 1;
+            }
+            sprite_y += sprite_dy;
+            BFN_SET(sprite->attr0, sprite_y, ATTR0_Y);
         }
         vid_vsync();
+        frame++;
     }
 
     return 0;
